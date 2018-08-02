ad3fd902dbf63027b99c8c6b3730f7c650403275
saitejasedate <saitejavelagandula@msitprogram.net>
2018-08-02 19:43:20 +0530

    " -> 0 of 4 passed. pylint: 7.69/10 "

--- cspp1-assignments/m4/p1/vowels_counter.py
+++ cspp1-assignments/m4/p1/vowels_counter.py
@@ -9,15 +9,14 @@ def main():
     # the input string is in s
     # remove pass and start your code here
     vowel_str = 'aeiou'
-    b = len(vowel_str)
-    l = len(input_str)
-    c = 0
-    j = 0
-    for i in range(0, l, 1):
-        for j in range(0, b, 1):
+    var_b = len(vowel_str)
+    var_l = len(input_str)
+    var_c = 0
+    for i in range(0, var_l, 1):
+        for j in range(0, var_b, 1):
             if input_str[i] == vowel_str[j]:
-                c = c+1
-    print(c)
+                var_c = var_c+1
+    print(var_c)
 
 if __name__ == "__main__":
     main()
