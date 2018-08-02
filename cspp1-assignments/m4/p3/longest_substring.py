f25de6fe6bbbf34ad95619c56622496f5ba1df19
saitejasedate <saitejavelagandula@msitprogram.net>
2018-08-02 22:45:07 +0530

    " -> 5 of 5 passed. pylint: 10.00/10 "

--- cspp1-assignments/m4/p3/longest_substring.py
+++ cspp1-assignments/m4/p3/longest_substring.py
@@ -13,7 +13,7 @@ For example, if s = 'abcbcd', then your program should print
 Longest substring in alphabetical order is: abc
 
 Note:This problem may be challenging. We encourage you to work smart.
-If you've spent more than a few hours on this problem, we suggest that you move on to 
+If you've spent more than a few hours on this problem, we suggest that you move on to
 a different part of the course.
 If you have time, come back to this problem after you've had a break and cleared your head.
 '''
