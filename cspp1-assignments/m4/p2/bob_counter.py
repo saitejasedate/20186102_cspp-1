9ca0453a94198185c9ea0d9d54b42b45e63ac5f6
saitejasedate <saitejavelagandula@msitprogram.net>
2018-08-02 21:05:48 +0530

    " -> 5 of 5 passed. pylint: 5.45/10 "

--- cspp1-assignments/m4/p2/bob_counter.py
+++ cspp1-assignments/m4/p2/bob_counter.py
@@ -9,7 +9,7 @@ def main():
     '''
     program to print number of times bob occurs in a string
     '''
-    str_lwr = raw_input() 
+    str_lwr = raw_input()
     len_str = len(str_lwr)
     i = 0
     c_len = 0
