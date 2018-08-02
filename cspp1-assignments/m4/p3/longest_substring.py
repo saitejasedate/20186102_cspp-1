90e450972ab40fb996abb9a84a0d3c9075ff95be
saitejasedate <saitejavelagandula@msitprogram.net>
2018-08-02 22:29:24 +0530

    " -> 0 of 5 passed. pylint: -3.08/10 "

--- cspp1-assignments/m4/p3/longest_substring.py
+++ cspp1-assignments/m4/p3/longest_substring.py
@@ -25,10 +25,10 @@ def main():
 		sub_str = input_str[i]
 		while i+1<len(input_str) and input_str[i] <= input_str[i+1]:
 			i=i+1
-			sub_str = input_str[i]+1
+			sub_str = input_str[i] + sub_str
 		if len(sub_str)>len(temp_str):
 			temp_str = sub_str
 	print(temp_str)
 
-if __name__== "__main__":
+if __name__ == "__main__":
 	main()
