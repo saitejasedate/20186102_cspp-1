dde33b694fbce714058ba46b5471107e863ebe8e
saitejasedate <saitejavelagandula@msitprogram.net>
2018-08-02 18:27:26 +0530

    " -> 0 of 4 passed. pylint: -6.43/10 "

--- cspp1-assignments/m4/p1/vowels_counter.py
+++ cspp1-assignments/m4/p1/vowels_counter.py
@@ -5,17 +5,17 @@
 #Number of vowels: 5
 
 def main():
-	s1 = input()
+	input_str = input()
 	# the input string is in s
 	# remove pass and start your code here
-	s2 = "a, e, i, o, u"
-	b = len(s2)
-	l = len(s1)
+	vowel_str = "a, e, i, o, u"
+	b = len(vowel_str)
+	l = len(input_str)
 	c = 0
 	j = 0
 	for i in range(0,l,1):
 		for j in range(0,b,1):
-			if s1[i] == s2[j]:
+			if input_str[i] == vowel_str[j]:
 				c = c+1
 	print(c)
 
