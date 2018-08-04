119e0cb6fab1025ab7db391ef797724ffe1c0ae8
saitejasedate <saitejavelagandula@msitprogram.net>
2018-08-04 11:03:26 +0530

    " -> 2 of 7 passed. pylint: 5.38/10 "

--- cspp1-assignments/m6/p1/fizz_buzz.py
+++ cspp1-assignments/m6/p1/fizz_buzz.py
@@ -1,4 +1,5 @@
 '''
+@author : saitejasedate
 Write a short program that prints each number from 1 to num on a new line.
 For each multiple of 3, print "Fizz" instead of the number.
 For each multiple of 5, print "Buzz" instead of the number.
@@ -10,15 +11,15 @@ def main():
     '''
     num_input = int(input())
     for i in range(1,num_input,1):
-    	if i%3==0 and i%5==0:
-    		print("Fizz")
-    		print("Buzz")
-    	elif i%3==0:
-    		print("Fizz")
-    	elif i%5==0:
-    		print("Buzz")
-    	else:
-    		print(i)
+        if i%3==0 and i%5==0:
+            print("Fizz")
+            print("Buzz")
+        elif i%3==0:
+            print("Fizz")
+        elif i%5==0:
+            print("Buzz")
+        else:
+            print(i)
 
 if __name__ == "__main__":
     main()
