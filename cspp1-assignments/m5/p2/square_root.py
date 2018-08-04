32f5f01f24e5b7079a2fc71234685d13304a902e
saitejasedate <saitejavelagandula@msitprogram.net>
2018-08-04 23:22:38 +0530

    " -> 3 of 4 passed. pylint: 10.00/10 "

--- cspp1-assignments/m5/p2/square_root.py
+++ cspp1-assignments/m5/p2/square_root.py
@@ -1,4 +1,7 @@
-# Write a python program to find the square root of the given number
+'''
+@author : saitejasedate
+'''
+# Write a python program to find the sq_uare root of the given number
 # using approximation method
 
 # testcase 1
@@ -10,15 +13,20 @@
 # output: 6.999999999999991
 
 def main():
-	EPSILON = 0.01
-	STEP = 0.1
-	SQUARE = int(input())
-	GUESS = 0.0
-	while GUESS <= SQUARE:
-	    if abs(GUESS**2-SQUARE) < EPSILON:
+	'''
+	python program to find the sq_uare root of the given number
+    using approximation method
+    '''
+	ep_silon = 0.01
+	st_ep = 0.1
+	sq_uare = int(input())
+	gu_ess= 0.0
+	while gu_ess<= sq_uare:
+	    if abs(gu_ess**2-sq_uare) < ep_silon:
 	        break
 	    else:
-	        GUESS += STEP
+	        gu_ess+= st_ep
+	print(str(gu_ess))
 
 if __name__ == "__main__":
 	main()
