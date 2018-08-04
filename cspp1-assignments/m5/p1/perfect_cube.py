912ee8cc657b4e7d44abd3f719c6cb99d831cf9f
saitejasedate <saitejavelagandula@msitprogram.net>
2018-08-04 23:07:17 +0530

    " -> 3 of 4 passed. pylint: 4.00/10 "

--- cspp1-assignments/m5/p1/perfect_cube.py
+++ cspp1-assignments/m5/p1/perfect_cube.py
@@ -10,17 +10,17 @@
 # Output: 21950 is not a perfect cube
 
 def main():
-	# input is captured in s
-	# watch out for the data type of value stored in s
-	# your code starts here
-	CUBE = int(input())
-	GUESS = 1
-	while (GUESS**3) < CUBE:
-	    GUESS += 1
-	if int(GUESS**3) == CUBE:
-	    print(CUBE, "is a perfect cube")
-	else:
-	    print(CUBE, "is not a perfect cube")
+    # input is captured in s
+    # watch out for the data type of value stored in s
+    # your code starts here
+    CUBE = int(input())
+    GUESS = 1
+    while (GUESS**3) < CUBE:
+        GUESS += 1
+    if int(GUESS**3) == CUBE:
+        print(CUBE, "is a perfect cube")
+    else:
+        print(CUBE, "is not a perfect cube")
 
 if __name__== "__main__":
-	main()
+    main()
