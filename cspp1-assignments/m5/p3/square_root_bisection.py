f529f0723e9a2d4b9f1dc0e641a1cf2ffed72a94
saitejasedate <saitejavelagandula@msitprogram.net>
2018-08-04 23:38:17 +0530

    " -> 3 of 3 passed. pylint: 7.86/10 "

--- cspp1-assignments/m5/p3/square_root_bisection.py
+++ cspp1-assignments/m5/p3/square_root_bisection.py
@@ -10,21 +10,21 @@
 # output: 6.999999999999991
 
 def main():
-	# ep_silon and step are initialized
-	# don't change these value
-	# your code starts here
-	x_num = int(input())
-	ep_silon = 0.01
-	lo_wer = 0.0
-	high_er = x_num
-	bi_val = (high_er + lo_wer)/2.0
-	while abs(bi_val**2 - x_num) > ep_silon:
-	    if bi_val**2 < x_num:
-	        lo_wer = bi_val
-	    else:
-	        high_er = bi_val
-	    bi_val = (high_er + lo_wer)/2.0
-	print(bi_val)
+    # ep_silon and step are initialized
+    # don't change these value
+    # your code starts here
+    x_num = int(input())
+    ep_silon = 0.01
+    lo_wer = 0.0
+    high_er = x_num
+    bi_val = (high_er + lo_wer)/2.0
+    while abs(bi_val**2 - x_num) > ep_silon:
+        if bi_val**2 < x_num:
+            lo_wer = bi_val
+        else:
+            high_er = bi_val
+        bi_val = (high_er + lo_wer)/2.0
+    print(bi_val)
 
 if __name__== "__main__":
-	main()
+    main()
