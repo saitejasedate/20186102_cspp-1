7dc7f95f2a4fcd84194da5a03c92a0ed92d94ac8
saitejasedate <saitejavelagandula@msitprogram.net>
2018-08-08 17:50:51 +0530

    " -> 1 of 9 passed. pylint: 6.50/10 "

--- cspp1-assignments/m9/p1/assignment1.py
+++ cspp1-assignments/m9/p1/assignment1.py
@@ -16,12 +16,12 @@ def is_word_guessed(secret_word, letters_guessed):
     '''
     # FILL IN YOUR CODE HERE...
     for i in range(len(secret_word)):
-        for j in range(len(list1)):
+        for j in range(len(letters_guessed)):
             a =list(secret_word)
-    if a[i]==letters_guessed[j]:
-        return True
-    else:
-        return False      
+            if a[i]==letters_guessed[j]:
+                return True
+            else:
+                return False      
 
 def main():
     '''
