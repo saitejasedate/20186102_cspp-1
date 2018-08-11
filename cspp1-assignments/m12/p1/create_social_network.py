b596db124f19a7d2bfc658a2483cea877bae7899
saitejasedate <saitejavelagandula@msitprogram.net>
2018-08-11 12:48:44 +0530

    " -> 1 of 3 passed. pylint: 10.00/10 "

--- cspp1-assignments/m12/p1/create_social_network.py
+++ cspp1-assignments/m12/p1/create_social_network.py
@@ -36,7 +36,7 @@ def create_social_network(data):
     out_dict = {}
     var_j = 0
     input_list = data.split()
-    if follow not in input_list:
+    if 'follow' not in input_list:
         return out_dict
     for var_j in range(0, (len(input_list)-2), 3):
         out_dict[input_list[var_j]] = input_list[var_j+2].split(",")
