7ba2158737e1579cadf547d7a38f9625087203ff
saitejasedate <saitejavelagandula@msitprogram.net>
2018-08-17 15:02:40 +0530

    " -> 1 of 3 passed. pylint: 6.73/10 "

--- cspp1-assignments/m16/CodeCampDocumentDistance/document_distance.py
+++ cspp1-assignments/m16/CodeCampDocumentDistance/document_distance.py
@@ -38,7 +38,7 @@ def create_dictionary(words_list):
 def clean_given_text(text_input):
     words = text_input.lower().strip().replace("\'","")
     regex = re.compile('[^a-z]')
-    words = regex.sub(" ",words).split(" ")
+    words = regex.sub("",words).split(" ")
     return words
 
 
