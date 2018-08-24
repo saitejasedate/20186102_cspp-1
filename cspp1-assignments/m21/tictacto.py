27f94d9612cee184c0ee11d86b0d290ef4e42cae
saitejasedate <saitejavelagandula@msitprogram.net>
2018-08-24 19:44:53 +0530

    " -> 12 of 14 passed. pylint: 6.98/10 "

--- cspp1-assignments/m21/tictacto.py
+++ cspp1-assignments/m21/tictacto.py
@@ -3,6 +3,7 @@ def is_input_valid(check_list):
     for i in check_list:
         for j in i:
             if j not in "x.o":
+                print("invalid input")
                 return False
             return True
 
