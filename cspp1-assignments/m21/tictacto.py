239385ac8e0b05d1587884d946d9f200fb60b052
saitejasedate <saitejavelagandula@msitprogram.net>
2018-08-24 11:53:30 +0530

    " -> 0 of 14 passed. pylint: 0/0 "

--- cspp1-assignments/m21/tictacto.py
+++ cspp1-assignments/m21/tictacto.py
@@ -3,7 +3,16 @@ def is_input_valid(check_list):
 	for i in check_list:
 		for j in i:
 			if j not in "x.o":
-				return invalid input
+				return False
+			return True
+
+def is_invalid_game(check_list):
+	if check_list.count('x') > 5 or check_list.count('o') > 5:
+		return False
+		elif check_list.count('x') - check_list.count('o') > 1 or check_list.count('o') - check_list.count('x') > 1:
+			return False
+	return True		
+
 
 
 def read_input():
