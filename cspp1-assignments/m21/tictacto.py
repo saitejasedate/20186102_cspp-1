c7a1da721bee2255cb6f28cc25665c1d573046a2
saitejasedate <saitejavelagandula@msitprogram.net>
2018-08-24 19:19:39 +0530

    " -> 8 of 14 passed. pylint: 6.81/10 "

--- cspp1-assignments/m21/tictacto.py
+++ cspp1-assignments/m21/tictacto.py
@@ -1,65 +1,65 @@
 def is_input_valid(check_list):
-	#print(check_list)
-	for i in check_list:
-		for j in i:
-			if j not in "x.o":
-				return 'invalid input'
-			return True
+    #print(check_list)
+    for i in check_list:
+        for j in i:
+            if j not in "x.o":
+                return 'invalid input'
+            return True
 
 def is_invalid_game(check_lists):
 
-	for check_list in check_lists:
-		if check_list.count('x') > 5 or check_list.count('o') > 5:
-			return 'invalid game'
-		# if abs(check_list.count('x') - check_list.count('o')) > 1:
-		# 	return False
-	return True
+    for check_list in check_lists:
+        if check_list.count('x') > 5 or check_list.count('o') > 5:
+            return 'invalid game'
+        # if abs(check_list.count('x') - check_list.count('o')) > 1:
+        #   return False
+    return True
 
 def is_check_var(check_list):
-	
-	transpose = zip(*check_list)
-	count_ = 1
-	variable = 'x'
-	def is_check_real(check_list):
-		nonlocal variable
-		nonlocal count_
-		for row in check_list:
-			if row.count(variable) == 3:
-				return True,variable		#print(True)
-		for row1 in transpose:
-			if row1.count(variable) == 3:
-				return True,variable		#print(True)
-		if (check_list[0][0] == check_list[1][1] == check_list[2][2] == variable) or\
-		(check_list[0][2] == check_list[1][1] == check_list[2][0] == variable):
-			return True,variable			#print(True)
-		else:
-			if count_  == 1:
-				variable = 'o'
-				count_ += 1
-				return is_check_real(check_list)
-	
-	return is_check_real(check_list)
+    
+    transpose = zip(*check_list)
+    count_ = 1
+    variable = 'x'
+    def is_check_real(check_list):
+        nonlocal variable
+        nonlocal count_
+        for row in check_list:
+            if row.count(variable) == 3:
+                return True,variable        #print(True)
+        for row1 in transpose:
+            if row1.count(variable) == 3:
+                return True,variable        #print(True)
+        if (check_list[0][0] == check_list[1][1] == check_list[2][2] == variable) or\
+        (check_list[0][2] == check_list[1][1] == check_list[2][0] == variable):
+            return True,variable            #print(True)
+        else:
+            if count_  == 1:
+                variable = 'o'
+                count_ += 1
+                return is_check_real(check_list)
+    
+    return is_check_real(check_list)
 
 def read_input():
-	tic_tac_to = []
-	for _ in range(3):
-		tic_tac_to.append(input().split(" "))
-	return tic_tac_to
+    tic_tac_to = []
+    for _ in range(3):
+        tic_tac_to.append(input().split(" "))
+    return tic_tac_to
 
 def main():
-	matrix = read_input()
-	input1 = is_input_valid(matrix)
-	if input1:
-		valid_res = is_invalid_game(matrix)	
-	if valid_res:
-		partial_res = is_check_var(matrix)
-		if partial_res[1] in 'xo':
-			print(partial_res[1])
-		else:
-		 	print("invalid input")
+    matrix = read_input()
+    input1 = is_input_valid(matrix)
+    if input1:
+        valid_res = is_invalid_game(matrix) 
+    if valid_res:
+        partial_res = is_check_var(matrix)
+        if partial_res[1] in 'xo':
+            print(partial_res[1])
+        else:
+            print("invalid input")
 
-	# else:
-	# 	print("invalid game")
+    # else:
+    #   print("invalid game")
 
 
 
