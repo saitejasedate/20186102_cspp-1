363fa93dc5ec775920b94f4086ea7f746e61ecc8
saitejasedate <saitejavelagandula@msitprogram.net>
2018-08-24 18:40:48 +0530

    " -> 0 of 14 passed. pylint: -1.59/10 "

--- cspp1-assignments/m21/tictacto.py
+++ cspp1-assignments/m21/tictacto.py
@@ -24,20 +24,20 @@ def is_check_var(check_list):
 		nonlocal count_
 		for row in check_list:
 			if row.count(variable) == 3:
-				return (True,variable)		#print(True)
+				return True		#print(True)
 		for row in transpose:
 			if row.count(variable) == 3:
-				return (True,variable)		#print(True)
+				return True		#print(True)
 		if (check_list[0][0] == check_list[1][1] == check_list[2][2] == variable) or\
 		(check_list[0][2] == check_list[1][1] == check_list[2][0] == variable):
-			return (True,variable)			#print(True)
+			return True			#print(True)
 		else:
 			if count_  == 1:
 				variable = 'o'
 				count_ += 1
 				is_check_real(check_list)
 	
-	is_check_real(check_list)
+	return is_check_real(check_list)
 
 def read_input():
 	tic_tac_to = []
@@ -51,7 +51,7 @@ def main():
 	if input1:
 		valid_res = is_invalid_game(matrix)	
 	if valid_res:
-		partial_res = is_check_var(matrix)[1]
+		partial_res = is_check_var(matrix)
 		print(partial_res)
 
 	# else:
@@ -61,3 +61,5 @@ def main():
 
 
 main()
+#a = [['x','o','.'],['x','x','x'],['x','o','x']]
+#print(is_check_var(a))
