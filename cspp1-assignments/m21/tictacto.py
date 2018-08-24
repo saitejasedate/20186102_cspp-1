488573557235b3be80812cfbad37ea104c5b7be9
saitejasedate <saitejavelagandula@msitprogram.net>
2018-08-24 19:53:20 +0530

    " -> 13 of 14 passed. pylint: 6.98/10 "

--- cspp1-assignments/m21/tictacto.py
+++ cspp1-assignments/m21/tictacto.py
@@ -57,6 +57,7 @@ def main():
         if valid_res:
             partial_res = is_check_var(matrix)
             if partial_res[1] in 'xo':
+                print(partial_res[1])
+            else:
                 print("draw")
-
 main()
