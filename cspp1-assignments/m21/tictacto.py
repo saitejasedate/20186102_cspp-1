703941e2453f62b64db5ceef5ba9e35476f77e3c
saitejasedate <saitejavelagandula@msitprogram.net>
2018-08-24 21:50:21 +0530

    " -> 14 of 14 passed. pylint: 9.27/10 "

--- cspp1-assignments/m21/tictacto.py
+++ cspp1-assignments/m21/tictacto.py
@@ -1,4 +1,10 @@
+'''
+program for tic-tac-toe game
+'''
 def is_input_valid(check_list):
+    '''
+    function to check if the input is valid
+    '''
     #print(check_list)
     for i in check_list:
         for j in i:
@@ -7,6 +13,9 @@ def is_input_valid(check_list):
     return True
 
 def is_invalid_game(check_lists):
+    '''
+    function to check if the game is valid
+    '''
     count_x_variable = 0
     count_o_variable = 0
     for row in check_lists:
@@ -17,7 +26,10 @@ def is_invalid_game(check_lists):
         return None
     return True
 
-def is_check_var(check_list):    
+def is_check_var(check_list):
+    '''
+    function to return the winner 
+    '''
     transpose = zip(*check_list)
     count_ = 1
     variable = 'x'
@@ -26,34 +38,40 @@ def is_check_var(check_list):
         nonlocal count_
         for row in check_list:
             if row.count(variable) == 3:
-                return True,variable        #print(True)
+                return True, variable        #print(True)
         for row1 in transpose:
             if row1.count(variable) == 3:
-                return True,variable        #print(True)
+                return True, variable        #print(True)
         if (check_list[0][0] == check_list[1][1] == check_list[2][2] == variable) or\
         (check_list[0][2] == check_list[1][1] == check_list[2][0] == variable):
-            return True,variable            #print(True)
+            return True, variable            #print(True)
         else:
-            if count_  == 1:
+            if count_ == 1:
                 variable = 'o'
                 count_ += 1
                 return is_check_real(check_list)
-    
+
     return is_check_real(check_list)
 
 def read_input():
+    '''
+    function to read input
+    '''
     tic_tac_to = []
     for _ in range(3):
         tic_tac_to.append(input().split(" "))
     return tic_tac_to
 
 def main():
+    '''
+    function to read input and call the required functions
+    '''
     matrix = read_input()
     input1 = is_input_valid(matrix)
     if input1 == False:
         print("invalid input")
     else:
-        valid_res = is_invalid_game(matrix) 
+        valid_res = is_invalid_game(matrix)
         if valid_res:
             partial_res = is_check_var(matrix)
             if partial_res is None:
